b1d12f842d1f53cd52911ad16d34d08abc6a0e0d
ImPaulinTech <impaulintech@gmail.com>
2022-02-22 23:42:24 +0800

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
+import Movies from "./components/movies";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <main className="container">
-        <h1>aaaaaaaaaaaa</h1>
-      </main>
-    );
+    return <Movies />;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom"; 
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-import Movies from './components/movies';
 
-ReactDOM.render(<Movies />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 reportWebVitals();
